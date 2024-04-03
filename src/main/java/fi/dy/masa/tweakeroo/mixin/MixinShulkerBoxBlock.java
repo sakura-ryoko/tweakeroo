ffaaa675e1ffb8610379f37d0714e474419281e2
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-04-03 15:48:38 -0400

    24w14a - java 21, shulker box tooltip slight Mixin change

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinShulkerBoxBlock.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinShulkerBoxBlock.java
@@ -2,23 +2,22 @@ package fi.dy.masa.tweakeroo.mixin;
 
 import java.util.List;
 
-import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.client.item.TooltipType;
+import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
-import net.minecraft.world.BlockView;
 import fi.dy.masa.tweakeroo.config.Configs;
 
 @Mixin(ShulkerBoxBlock.class)
 public abstract class MixinShulkerBoxBlock
 {
     @Inject(method = "appendTooltip", at = @At("HEAD"), cancellable = true)
-    private void removeVanillaTooltip(ItemStack stack, BlockView world, List<Text> tooltip, TooltipContext options, DynamicRegistryManager registryManager, CallbackInfo ci)
+    private void removeVanillaTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options, CallbackInfo ci)
     {
         if (Configs.Disable.DISABLE_SHULKER_BOX_TOOLTIP.getBooleanValue())
         {
