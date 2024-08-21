6e9ee88a4854ebb948452b222a8131f9501034ad
Sakura Ryoko <sakura@ryoko.soy>
2024-08-21 00:22:28 -0400

    update to yarn build 27

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWeatherRendering.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinWeatherRendering.java
@@ -1,9 +1,9 @@
 package fi.dy.masa.tweakeroo.mixin;
 
-import net.minecraft.client.option.ParticlesMode;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.WeatherRendering;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.ParticlesMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,12 +16,11 @@ public class MixinWeatherRendering
 {
     // TODO --> tickRainSplashing
     @Inject(method = "method_62319", at = @At("HEAD"), cancellable = true) // renderRain
-    private void cancelRainRender(ClientWorld clientWorld, Camera camera, int i, ParticlesMode particlesMode, CallbackInfo ci)
+    private void cancelRainRender(ClientWorld world, Camera camera, int ticks, ParticlesMode particlesMode, CallbackInfo ci)
     {
         if (Configs.Disable.DISABLE_RAIN_EFFECTS.getBooleanValue())
         {
             ci.cancel();
         }
     }
-
 }
