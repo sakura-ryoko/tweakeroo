756850b8fe4382e96b7dcadf7e4046d8a9a5f142
Sakura Ryoko <sakura@ryoko.soy>
2024-09-25 14:11:21 -0400

    port to 24w39a

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinAbstractInventoryScreen.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinAbstractInventoryScreen.java
@@ -8,18 +8,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.screen.ScreenHandler;
-import net.minecraft.text.Text;
 
 import fi.dy.masa.tweakeroo.config.Configs;
 
 @Mixin(AbstractInventoryScreen.class)
-public abstract class MixinAbstractInventoryScreen<T extends ScreenHandler> extends HandledScreen<T>
+public abstract class MixinAbstractInventoryScreen
 {
-    private MixinAbstractInventoryScreen(T container, PlayerInventory playerInventory, Text textComponent)
+    private MixinAbstractInventoryScreen(HandledScreen<?> handledScreen)
     {
-        super(container, playerInventory, textComponent);
+        super();
     }
 
     @Inject(method = "drawStatusEffects", at = @At("HEAD"), cancellable = true)
