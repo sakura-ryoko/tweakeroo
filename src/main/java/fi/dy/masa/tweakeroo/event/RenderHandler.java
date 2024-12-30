88154f3f880d0872ec12e65afa1606e721e22bdc
Sakura Ryoko <sakura@ryoko.soy>
2024-12-30 00:42:41 -0500

    fix: Remove the wrong "Pair" and use Apache Commons feat: add New InventoryOverlayHandler system; which now makes Tweakeroo's InventoryPreview work the same as MiniHUD.

--- src/main/java/fi/dy/masa/tweakeroo/event/RenderHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/event/RenderHandler.java
@@ -1,6 +1,6 @@
 package fi.dy.masa.tweakeroo.event;
 
-import com.llamalad7.mixinextras.lib.apache.commons.tuple.Pair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.joml.Matrix4f;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -25,7 +25,6 @@ import net.minecraft.world.World;
 
 import fi.dy.masa.malilib.gui.GuiBase;
 import fi.dy.masa.malilib.interfaces.IRenderer;
-import fi.dy.masa.malilib.render.InventoryOverlay;
 import fi.dy.masa.malilib.util.ActiveMode;
 import fi.dy.masa.malilib.util.Color4f;
 import fi.dy.masa.malilib.util.InventoryUtils;
@@ -35,8 +34,8 @@ import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.config.Hotkeys;
 import fi.dy.masa.tweakeroo.data.ServerDataSyncer;
+import fi.dy.masa.tweakeroo.renderer.InventoryOverlayHandler;
 import fi.dy.masa.tweakeroo.renderer.RenderUtils;
-import fi.dy.masa.tweakeroo.util.RayTraceUtils;
 
 public class RenderHandler implements IRenderer
 {
@@ -70,12 +69,16 @@ public class RenderHandler implements IRenderer
         if (FeatureToggle.TWEAK_INVENTORY_PREVIEW.getBooleanValue() &&
             Hotkeys.INVENTORY_PREVIEW.getKeybind().isKeybindHeld())
         {
+            /*
             InventoryOverlay.Context context = RayTraceUtils.getTargetInventory(mc);
 
             if (context != null)
             {
                 RenderUtils.renderInventoryOverlay(context, drawContext);
             }
+             */
+
+            InventoryOverlayHandler.getInstance().getRenderContext(drawContext, profiler, mc);
         }
 
         if (FeatureToggle.TWEAK_PLAYER_INVENTORY_PEEK.getBooleanValue() &&
@@ -135,7 +138,7 @@ public class RenderHandler implements IRenderer
 
                 if (player != null)
                 {
-                    Pair<Entity, NbtCompound> pair = ServerDataSyncer.getInstance().requestEntity(player.getId());
+                    Pair<Entity, NbtCompound> pair = ServerDataSyncer.getInstance().requestEntity(world, player.getId());
                     NbtCompound nbt = new NbtCompound();
                     EnderChestInventory inv;
 
