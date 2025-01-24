3db4c2fe6c1b9f6f04267a02a020cfd32bdc7a26
Sakura Ryoko <sakura@ryoko.soy>
2025-01-23 23:01:16 -0500

    port to 25w04a

--- src/main/java/fi/dy/masa/tweakeroo/mixin/item/MixinItemStack.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/item/MixinItemStack.java
@@ -1,21 +1,33 @@
 package fi.dy.masa.tweakeroo.mixin.item;
 
+import java.util.function.Consumer;
+
 import fi.dy.masa.malilib.util.InventoryUtils;
+import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.BlockItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack
 {
     @Shadow public abstract Item getItem();
+
+    @Shadow public abstract boolean itemMatches(RegistryEntry<Item> itemEntry);
 
     @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
     public void getMaxStackSizeStackSensitive(CallbackInfoReturnable<Integer> cir)
@@ -26,6 +38,18 @@ public abstract class MixinItemStack
             InventoryUtils.shulkerBoxHasItems((ItemStack) (Object) this) == false)
         {
             cir.setReturnValue(64);
+        }
+    }
+
+    @Inject(method = "appendComponentTooltip", at = @At("HEAD"), cancellable = true)
+    private <T> void tweakeroo_removeVanillaTooltip(ComponentType<T> componentType, Item.TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type, CallbackInfo ci)
+    {
+        if (this.getItem() instanceof BlockItem block &&
+            block.getBlock() instanceof ShulkerBoxBlock &&
+            componentType == DataComponentTypes.CONTAINER &&
+            Configs.Disable.DISABLE_SHULKER_BOX_TOOLTIP.getBooleanValue())
+        {
+            ci.cancel();
         }
     }
 }
