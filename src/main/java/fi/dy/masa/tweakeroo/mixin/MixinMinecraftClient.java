43edfebce298fdcdf6c2b6736063a6a39d538a7a
Sakura Ryoko <sakura@ryoko.soy>
2025-02-07 16:26:53 -0500

    port to 1.21.3

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMinecraftClient.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinMinecraftClient.java
@@ -37,8 +37,8 @@ public abstract class MixinMinecraftClient implements IMinecraftClientInvoker
     @Shadow @Final public GameOptions options;
     @Shadow private int itemUseCooldown;
     @Shadow protected int attackCooldown;
-    
-    @Shadow private boolean doAttack() { return false; }
+
+    @Shadow private boolean doAttack() {return false;}
     @Shadow private void doItemUse() {}
 
     @Override
