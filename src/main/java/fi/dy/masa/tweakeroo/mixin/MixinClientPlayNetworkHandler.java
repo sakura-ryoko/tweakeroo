8e8d3a7f4f71c8834592d48348ebf4ed1fe4a622
Sakura Ryoko <sakura@ryoko.soy>
2025-02-06 21:17:33 -0500

    add: selective rendering system from Tweak Fork.  Note that this tends to be a bit laggy / odd to use.

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinClientPlayNetworkHandler.java
@@ -1,18 +1,24 @@
 package fi.dy.masa.tweakeroo.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientCommonNetworkHandler;
 import net.minecraft.client.network.ClientConnectionState;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.CustomPayload;
-import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
-import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
-import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
+import net.minecraft.network.packet.s2c.play.*;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,14 +26,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.data.DataManager;
 import fi.dy.masa.tweakeroo.tweaks.PlacementTweaks;
+import fi.dy.masa.tweakeroo.tweaks.RenderTweaks;
 import fi.dy.masa.tweakeroo.util.MiscUtils;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class MixinClientPlayNetworkHandler extends ClientCommonNetworkHandler
 {
+    @Shadow private ClientWorld world;
+    @Shadow private int simulationDistance;
+
     protected MixinClientPlayNetworkHandler(MinecraftClient client, ClientConnection connection, ClientConnectionState connectionState)
     {
         super(client, connection, connectionState);
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onOpenScreen", at = @At("HEAD"), cancellable = true)
+    private void onOpenScreenListener(OpenScreenS2CPacket packet, CallbackInfo ci)
+    {
+        if (!RenderTweaks.onOpenScreen(packet.getName(), packet.getScreenHandlerType(), packet.getSyncId()))
+        {
+            ci.cancel();
+        }
     }
 
     @Inject(method = "onScreenHandlerSlotUpdate", at = @At(
@@ -43,7 +65,7 @@ public abstract class MixinClientPlayNetworkHandler extends ClientCommonNetworkH
     }
 
     @Inject(method = "onDeathMessage", at = @At(value = "INVOKE", // onCombatEvent
-            target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
+                                                target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
     private void onPlayerDeath(DeathMessageS2CPacket packetIn, CallbackInfo ci)
     {
         MinecraftClient mc = MinecraftClient.getInstance();
@@ -69,7 +91,7 @@ public abstract class MixinClientPlayNetworkHandler extends ClientCommonNetworkH
 
     @Inject(method = "onEntityStatus",
             at = @At(value = "INVOKE", ordinal = 0,
-            target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;getActiveDeathProtector(Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/item/ItemStack;"))
+                     target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;getActiveDeathProtector(Lnet/minecraft/entity/player/PlayerEntity;)Lnet/minecraft/item/ItemStack;"))
     private void onPlayerUseTotemOfUndying(EntityStatusS2CPacket packet, CallbackInfo ci)
     {
         if (this.client.player == null)
@@ -89,5 +111,102 @@ public abstract class MixinClientPlayNetworkHandler extends ClientCommonNetworkH
                 }
             }
         }
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onPlayerRespawn", at = @At(value = "NEW",
+                                                 target = "net/minecraft/client/world/ClientWorld"))
+    private void onPlayerRespawnInject(PlayerRespawnS2CPacket packet, CallbackInfo ci)
+    {
+        RenderTweaks.resetWorld(this.simulationDistance);
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onGameJoin", at = @At(value = "NEW",
+                                            target = "net/minecraft/client/world/ClientWorld"))
+    private void onGameJoinInject(GameJoinS2CPacket packet, CallbackInfo ci)
+    {
+        RenderTweaks.resetWorld(this.simulationDistance);
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onChunkData", at = @At("RETURN"))
+    private void onChunkDataInject(ChunkDataS2CPacket packet, CallbackInfo ci)
+    {
+        int cx = packet.getChunkX();
+        int cz = packet.getChunkZ();
+        RenderTweaks.loadFakeChunk(cx, cz);
+
+        if (!FeatureToggle.TWEAK_SELECTIVE_BLOCKS_RENDERING.getBooleanValue())
+        {
+            return;
+        }
+        WorldChunk worldChunk = this.world.getChunkManager().getWorldChunk(cx, cz);
+
+        if (worldChunk != null)
+        {
+            BlockPos.Mutable pos = new BlockPos.Mutable();
+            ChunkSection[] sections = worldChunk.getSectionArray();
+            for (int i = 0; i < sections.length; i++)
+            {
+                ChunkSection section = sections[i];
+                if (section != null && !section.isEmpty())
+                {
+                    for (int x = 0; x < 16; x++)
+                    {
+                        for (int y = 0; y < 16; y++)
+                        {
+                            for (int z = 0; z < 16; z++)
+                            {
+                                pos.set(x + worldChunk.getPos().getStartX(), y + this.world.sectionIndexToCoord(i), z + worldChunk.getPos().getStartZ());
+
+                                if (!RenderTweaks.isPositionValidForRendering(pos))
+                                {
+                                    BlockEntity be = worldChunk.getBlockEntity(pos);
+                                    BlockState state = section.getBlockState(x, y, z);
+                                    worldChunk.setBlockState(pos, Blocks.AIR.getDefaultState(), -1);
+                                    RenderTweaks.setFakeBlockState(this.world, pos, state, be);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onUnloadChunk", at = @At("RETURN"))
+    private void onUnloadChunkInject(UnloadChunkS2CPacket packet, CallbackInfo ci)
+    {
+        int i = packet.pos().x;
+        int j = packet.pos().z;
+        RenderTweaks.unloadFakeChunk(i, j);
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onChunkLoadDistance", at = @At("RETURN"))
+    private void onChunkLoadDistanceInject(ChunkLoadDistanceS2CPacket packet, CallbackInfo ci)
+    {
+        RenderTweaks.getFakeWorld().getChunkManager().updateLoadDistance(packet.getDistance());
+    }
+
+    /**
+     * Copied From Tweak Fork by Andrew54757
+     */
+    @Inject(method = "onChunkRenderDistanceCenter", at = @At("RETURN"))
+    private void onChunkRenderDistanceCenterInject(ChunkRenderDistanceCenterS2CPacket packet, CallbackInfo ci)
+    {
+        RenderTweaks.getFakeWorld().getChunkManager().setChunkMapCenter(packet.getChunkX(), packet.getChunkZ());
     }
 }
