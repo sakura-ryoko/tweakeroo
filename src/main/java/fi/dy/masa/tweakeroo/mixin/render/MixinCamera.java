1cddc0893da296244a27415fc1d3d28f8fcf7ddf
Sakura Ryoko <sakura@ryoko.soy>
2025-04-25 18:02:49 -0400

    few freecam mixin tweaks

--- src/main/java/fi/dy/masa/tweakeroo/mixin/render/MixinCamera.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/render/MixinCamera.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import fi.dy.masa.malilib.gui.GuiBase;
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
 @Mixin(Camera.class)
@@ -16,8 +15,7 @@ public class MixinCamera
     @Inject(method = "getSubmersionType", at = @At("HEAD"), cancellable = true)
     private void tweakeroo_disableFluidFog(CallbackInfoReturnable<CameraSubmersionType> cir)
     {
-        if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
-            GuiBase.isShiftDown())
+        if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue())
         {
             cir.setReturnValue(CameraSubmersionType.NONE);
         }
