82c0daa9aaeeb1f99824aac4b3eec4b85853c8a5
Sakura Ryoko <sakura@ryoko.soy>
2025-06-21 19:41:01 -0400

    fix data from non-servux

--- src/main/java/fi/dy/masa/tweakeroo/data/ServerDataSyncer.java
+++ src/main/java/fi/dy/masa/tweakeroo/data/ServerDataSyncer.java
@@ -490,10 +490,8 @@ public class ServerDataSyncer implements IClientTickHandler, IDataSyncer
             {
                 this.pendingBlockEntitiesQueue.add(pos);
             }
-            else
-            {
-                return this.refreshBlockEntityFromWorld(world, pos);
-            }
+
+            return this.refreshBlockEntityFromWorld(world, pos);
         }
 
         return null;
@@ -551,12 +549,8 @@ public class ServerDataSyncer implements IClientTickHandler, IDataSyncer
         {
             this.pendingEntitiesQueue.add(entityId);
         }
-        else
-        {
-            return this.refreshEntityFromWorld(world, entityId);
-        }
 
-        return null;
+        return this.refreshEntityFromWorld(world, entityId);
     }
 
     private @Nullable Pair<Entity, NbtCompound> refreshEntityFromWorld(World world, int entityId)
