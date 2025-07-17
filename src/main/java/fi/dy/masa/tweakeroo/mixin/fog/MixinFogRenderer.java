c0c3893fc2c6fdb92e72cb4fba0c27b069714181
xiej2520 <jackyxie2520@outlook.com>
2025-07-17 09:15:37 -0500

    fix: tweakMatchingSkyFog should only affect sky fog. (#130)

--- src/main/java/fi/dy/masa/tweakeroo/mixin/fog/MixinFogRenderer.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/fog/MixinFogRenderer.java
@@ -1,19 +1,13 @@
 package fi.dy.masa.tweakeroo.mixin.fog;
 
-import org.joml.Vector4f;
-
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.fog.FogRenderer;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import fi.dy.masa.tweakeroo.config.Configs;
-import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
 @Mixin(FogRenderer.class)
 public class MixinFogRenderer
@@ -24,25 +18,6 @@ public class MixinFogRenderer
     private float tweakeroo_disableSkyDarkness(ClientWorld.Properties instance)
     {
         return Configs.Disable.DISABLE_SKY_DARKNESS.getBooleanValue() ? 1.0F : instance.getVoidDarknessRange();
-    }
-
-    @Inject(method = "getFogColor", at = @At("HEAD"), cancellable = true)
-    private void tweakeroo_adjustFogColor(Camera camera, float tickProgress, ClientWorld world, int viewDistance, float skyDarkness, boolean thick, CallbackInfoReturnable<Vector4f> cir)
-    {
-        if (FeatureToggle.TWEAK_MATCHING_SKY_FOG.getBooleanValue())
-        {
-            if (world.getDimension().hasSkyLight())
-            {
-                int color = world.getSkyColor(camera.getPos(), skyDarkness);
-
-                cir.setReturnValue(new Vector4f(
-                        ColorHelper.getRedFloat(color),
-                        ColorHelper.getGreenFloat(color),
-                        ColorHelper.getBlueFloat(color),
-                        ColorHelper.getAlphaFloat(color))
-                );
-            }
-        }
     }
 
     @ModifyConstant(method = "applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;",
