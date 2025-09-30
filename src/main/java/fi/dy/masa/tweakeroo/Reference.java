a2ed50ee9badb01a3b76b80decc546e60e9a8783
Sakura Ryoko <sakura@ryoko.soy>
2025-09-30 12:04:40 -0400

    Bump to Release

--- src/main/java/fi/dy/masa/tweakeroo/Reference.java
+++ src/main/java/fi/dy/masa/tweakeroo/Reference.java
@@ -1,7 +1,6 @@
 package fi.dy.masa.tweakeroo;
 
-import net.minecraft.MinecraftVersion;
-
+import net.minecraft.SharedConstants;
 import fi.dy.masa.malilib.util.StringUtils;
 
 public class Reference
@@ -9,7 +8,7 @@ public class Reference
     public static final String MOD_ID = "tweakeroo";
     public static final String MOD_NAME = "Tweakeroo";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
-    public static final String MC_VERSION = MinecraftVersion.CURRENT.name();
+    public static final String MC_VERSION = SharedConstants.getGameVersion().id();
     public static final String MOD_TYPE = "fabric";
     public static final String MOD_STRING = MOD_ID+"-"+MOD_TYPE+"-"+MC_VERSION+"-"+MOD_VERSION;
 }
