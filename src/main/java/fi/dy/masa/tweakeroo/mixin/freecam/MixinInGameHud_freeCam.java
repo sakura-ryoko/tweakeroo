6be863b3c56fd2466f735ade244684e3885c0c57
Sakura Ryoko <sakura@ryoko.soy>
2025-10-12 15:23:43 -0400

    add: `freeCameraShowStatusBars` which disables the "status bars" such as Health, EXP, and the locator bar while using FreeCam.

--- src/main/java/fi/dy/masa/tweakeroo/mixin/freecam/MixinInGameHud_freeCam.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/freecam/MixinInGameHud_freeCam.java
@@ -35,10 +35,56 @@ public abstract class MixinInGameHud_freeCam
     @Inject(method = "renderHotbar", at = @At("HEAD"), cancellable = true)
     public void tweakeroo_overrideHotbarRendering(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
 	{
+		// This turns off rendering of the hotbar
         if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
 			!Configs.Generic.FREE_CAMERA_SHOW_HOTBAR.getBooleanValue())
 		{
             ci.cancel();
         }
     }
+
+	@Inject(method = "renderHeldItemTooltip", at = @At("HEAD"), cancellable = true)
+	public void tweakeroo_overrideHeldItemTooltipRendering(DrawContext context, CallbackInfo ci)
+	{
+		// This turns off rendering of the item "tooltips" when selecting hotbar items
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_HOTBAR.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_HANDS.getBooleanValue())
+		{
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "renderStatusBars", at = @At("HEAD"), cancellable = true)
+	public void tweakeroo_overrideStatusBarRendering1(DrawContext context, CallbackInfo ci)
+	{
+		// This turns off all status bars
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_STATUS_BARS.getBooleanValue())
+		{
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "renderMountHealth", at = @At("HEAD"), cancellable = true)
+	public void tweakeroo_overrideStatusBarRendering2(DrawContext context, CallbackInfo ci)
+	{
+		// This turns off the "mount health" status bar
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_STATUS_BARS.getBooleanValue())
+		{
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "getCurrentBarType", at = @At("HEAD"), cancellable = true)
+	public void tweakeroo_overrideExpBarRendering(CallbackInfoReturnable<InGameHud.BarType> cir)
+	{
+		// This turns off all status bars
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_STATUS_BARS.getBooleanValue())
+		{
+			cir.setReturnValue(InGameHud.BarType.EMPTY);
+		}
+	}
 }
---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/freecam/MixinLocatorBar_freeCam.java
@@ -0,0 +1,36 @@
+package fi.dy.masa.tweakeroo.mixin.freecam;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.bar.LocatorBar;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import fi.dy.masa.tweakeroo.config.Configs;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+
+@Mixin(value = LocatorBar.class, priority = 999)
+public class MixinLocatorBar_freeCam
+{
+	@Inject(method = "renderBar", at = @At("HEAD"), cancellable = true)
+	private void tweakeroo_disableLocatorBar(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
+	{
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_STATUS_BARS.getBooleanValue())
+		{
+			ci.cancel();
+		}
+	}
+
+	@Inject(method = "renderAddons", at = @At("HEAD"), cancellable = true)
+	private void tweakeroo_disableLocatorBarAddons(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci)
+	{
+		if (FeatureToggle.TWEAK_FREE_CAMERA.getBooleanValue() &&
+			!Configs.Generic.FREE_CAMERA_SHOW_STATUS_BARS.getBooleanValue())
+		{
+			ci.cancel();
+		}
+	}
+}
