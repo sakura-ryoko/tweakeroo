c0fed7c1cdcba94493e853bda53a897a9524d5ef
Sakura Ryoko <sakura@ryoko.soy>
2025-12-29 15:11:23 -0500

    fix: Zoom and Elytra Camera

--- src/main/java/fi/dy/masa/tweakeroo/mixin/render/MixinGameRenderer.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/render/MixinGameRenderer.java
@@ -1,5 +1,10 @@
 package fi.dy.masa.tweakeroo.mixin.render;
 
+import net.minecraft.client.Camera;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,21 +21,18 @@ import fi.dy.masa.tweakeroo.config.FeatureToggle;
 import fi.dy.masa.tweakeroo.config.Hotkeys;
 import fi.dy.masa.tweakeroo.util.CameraUtils;
 import fi.dy.masa.tweakeroo.util.MiscUtils;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.world.entity.Entity;
 
 @Mixin(value = GameRenderer.class, priority = 1001)
 public abstract class MixinGameRenderer
 {
     @Shadow @Final private Minecraft minecraft;
+    @Shadow public abstract void updateCamera(DeltaTracker deltaTracker);
 
     @Unique private float realYaw;
     @Unique private float realPitch;
 
     @Inject(method = "renderLevel", at = @At("HEAD"), cancellable = true)
-    private void tweakeroo_onRenderWorld(CallbackInfo ci)
+    private void tweakeroo_onRenderWorld(DeltaTracker deltaTracker, CallbackInfo ci)
     {
         if (Callbacks.skipWorldRendering)
         {
@@ -39,7 +41,7 @@ public abstract class MixinGameRenderer
     }
 
     @Inject(method = "getFov", at = @At("HEAD"), cancellable = true)
-    private void tweakeroo_applyZoom(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Float> cir)
+    private void tweakeroo_applyZoom(Camera camera, float f, boolean bl, CallbackInfoReturnable<Float> cir)
     {
         if (MiscUtils.isZoomActive())
         {
@@ -50,7 +52,7 @@ public abstract class MixinGameRenderer
     @Inject(method = "renderLevel", at = @At(
                 value = "INVOKE", shift = Shift.AFTER,
                 target = "Lnet/minecraft/client/renderer/GameRenderer;pick(F)V"))
-    private void tweakeroo_overrideRenderViewEntityPre(CallbackInfo ci)
+    private void tweakeroo_overrideRenderViewEntityPre(DeltaTracker deltaTracker, CallbackInfo ci)
     {
         if (FeatureToggle.TWEAK_ELYTRA_CAMERA.getBooleanValue() && Hotkeys.ELYTRA_CAMERA.getKeybind().isKeybindHeld())
         {
@@ -61,12 +63,13 @@ public abstract class MixinGameRenderer
                 this.realYaw = entity.getYRot();
                 this.realPitch = entity.getXRot();
                 MiscUtils.setEntityRotations(entity, CameraUtils.getCameraYaw(), CameraUtils.getCameraPitch());
+                this.updateCamera(deltaTracker);
             }
         }
     }
 
     @Inject(method = "renderLevel", at = @At("RETURN"))
-    private void tweakeroo_overrideRenderViewEntityPost(CallbackInfo ci)
+    private void tweakeroo_overrideRenderViewEntityPost(DeltaTracker deltaTracker, CallbackInfo ci)
     {
         if (FeatureToggle.TWEAK_ELYTRA_CAMERA.getBooleanValue() && Hotkeys.ELYTRA_CAMERA.getKeybind().isKeybindHeld())
         {
@@ -75,6 +78,7 @@ public abstract class MixinGameRenderer
             if (entity != null)
             {
                 MiscUtils.setEntityRotations(entity, this.realYaw, this.realPitch);
+                this.updateCamera(deltaTracker);
             }
         }
     }
