29e2aa5c15b8bb10a11a02523f0430d7fcf954e9
Sakura Ryoko <sakura@ryoko.soy>
2025-12-29 11:59:11 -0500

    fix: `tweakAutoSwitchElytra` from version sync (Tested)
    fix: `tweakAutoSwitchElytra` from version sync (Tested)
    
    fix: `tweakAutoSwitchElytra` from version sync (Tested)
    
    revert

--- src/main/java/fi/dy/masa/tweakeroo/util/InventoryUtils.java
+++ src/main/java/fi/dy/masa/tweakeroo/util/InventoryUtils.java
@@ -1102,6 +1102,13 @@ public class InventoryUtils
         }
     }
 
+    // todo for easier forwards porting when the `ArmorItem` disappears
+    private static boolean checkChestSlot(ItemStack stack)
+    {
+//        return stack.getItem() instanceof ArmorItem && EquipmentUtils.matchArmorSlot(stack, EquipmentSlot.CHEST);
+        return EquipmentUtils.matchArmorSlot(stack, EquipmentSlot.CHEST);
+    }
+
     public static void swapElytraAndChestPlate(@Nullable PlayerEntity player)
     {
         if (player == null || GuiUtils.getCurrentScreen() != null)
@@ -1112,10 +1119,7 @@ public class InventoryUtils
         ScreenHandler container = player.currentScreenHandler;
         ItemStack currentStack = player.getEquippedStack(EquipmentSlot.CHEST);
 
-        Predicate<ItemStack> stackFilterChestPlate = (s) -> EquipmentUtils.matchArmorSlot(s, EquipmentSlot.CHEST);
-                //s.getItem() instanceof ArmorItem &&
-                //((ArmorItem) s.getItem()).getSlotType() == EquipmentSlot.CHEST;
-                //s.get(DataComponentTypes.EQUIPPABLE).slot() == EquipmentSlot.CHEST;
+        Predicate<ItemStack> stackFilterChestPlate = (s) -> checkChestSlot(s);
 
         if (currentStack.isEmpty() || stackFilterChestPlate.test(currentStack))
         {
