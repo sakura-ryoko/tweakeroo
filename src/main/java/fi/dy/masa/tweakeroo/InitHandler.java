6166cb4636a2f680ecd595a3d37028d790d33d7f
BytelyPlay <universalnebula2024@outlook.com>
2026-01-17 10:16:12 +0100

    Small Fix, this can likely also just be done if you remove the setRealTickRate call in onWorldLoadPost

--- src/main/java/fi/dy/masa/tweakeroo/InitHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/InitHandler.java
@@ -13,11 +13,9 @@ import fi.dy.masa.tweakeroo.config.Callbacks;
 import fi.dy.masa.tweakeroo.config.Configs;
 import fi.dy.masa.tweakeroo.data.DataManager;
 import fi.dy.masa.tweakeroo.data.EntityDataManager;
-import fi.dy.masa.tweakeroo.event.ClientTickHandler;
-import fi.dy.masa.tweakeroo.event.InputHandler;
-import fi.dy.masa.tweakeroo.event.RenderHandler;
-import fi.dy.masa.tweakeroo.event.WorldLoadListener;
+import fi.dy.masa.tweakeroo.event.*;
 import fi.dy.masa.tweakeroo.gui.GuiConfigs;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
 import net.minecraft.client.Minecraft;
 
 public class InitHandler implements IInitializationHandler
@@ -51,5 +49,7 @@ public class InitHandler implements IInitializationHandler
 
 	    ClientCommandHandler.INSTANCE.registerCommand(new FcCommand());
         Callbacks.init(Minecraft.getInstance());
+
+        ClientLoginConnectionEvents.DISCONNECT.register(new LoginDisconnectListener());
     }
 }
