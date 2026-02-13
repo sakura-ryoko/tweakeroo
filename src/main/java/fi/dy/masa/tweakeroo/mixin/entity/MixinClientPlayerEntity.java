c9af0a23654d494f0301277eaed62b695ae8a279
James Garratt <jngarratt@gmail.com>
2026-02-13 14:18:11 +1100

    set autoSwitchElytraChestplate to current chestplate when switching to elytra

--- src/main/java/fi/dy/masa/tweakeroo/mixin/entity/MixinClientPlayerEntity.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/entity/MixinClientPlayerEntity.java
@@ -125,6 +125,7 @@ public abstract class MixinClientPlayerEntity extends AbstractClientPlayer
                 if (!this.getItemBySlot(EquipmentSlot.CHEST).is(Items.ELYTRA) ||
                     this.getItemBySlot(EquipmentSlot.CHEST).getDamageValue() > this.getItemBySlot(EquipmentSlot.CHEST).getMaxDamage() - 10)
                 {
+                    this.autoSwitchElytraChestplate = this.getItemBySlot(EquipmentSlot.CHEST);
                     InventoryUtils.equipBestElytra(this);
                 }
             }
@@ -154,6 +155,10 @@ public abstract class MixinClientPlayerEntity extends AbstractClientPlayer
                             if (targetSlot >= 0)
                             {
                                 InventoryUtils.swapItemToEquipmentSlot(this, EquipmentSlot.CHEST, targetSlot);
+                                this.autoSwitchElytraChestplate = ItemStack.EMPTY;
+                            } else {
+                                // cached item not found, try to swap to the default chest plate.
+                                InventoryUtils.swapElytraAndChestPlate(this);
                                 this.autoSwitchElytraChestplate = ItemStack.EMPTY;
                             }
                         }
