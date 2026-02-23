4d32caba7d9591fc8a9395739a5c6ea52450de36
Serge <sgerodes@gmail.com>
2026-02-23 12:21:47 +0100

    feat: prevent powder snow FOV zoom when disableFreezeOverlay is enabled

---
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/entity/MixinAbstractClientPlayer.java
@@ -0,0 +1,44 @@
+package fi.dy.masa.tweakeroo.mixin.entity;
+
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import fi.dy.masa.tweakeroo.config.Configs;
+
+@Mixin(AbstractClientPlayer.class)
+public abstract class MixinAbstractClientPlayer
+{
+	@Redirect(method = "getFieldOfViewModifier",
+			  at = @At(value = "INVOKE",
+					   target = "Lnet/minecraft/client/player/AbstractClientPlayer;getAttributeValue(Lnet/minecraft/core/Holder;)D"))
+	private double tweakeroo$disableFreezeFovChange(AbstractClientPlayer instance, Holder<Attribute> attribute)
+	{
+		if (Configs.Disable.DISABLE_FREEZE_OVERLAY.getBooleanValue())
+		{
+			AttributeInstance attrInstance = instance.getAttribute(attribute);
+
+			if (attrInstance != null)
+			{
+				Identifier powderSnowId = Identifier.withDefaultNamespace("powder_snow");
+				AttributeModifier modifier = attrInstance.getModifier(powderSnowId);
+
+				if (modifier != null)
+				{
+					attrInstance.removeModifier(powderSnowId);
+					double cleanValue = attrInstance.getValue();
+					attrInstance.addTransientModifier(modifier);
+					return cleanValue;
+				}
+			}
+		}
+
+		return instance.getAttributeValue(attribute);
+	}
+}
